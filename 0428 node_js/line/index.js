32ec922d71ae75b34b63c7fa705f65639f4e9c07
Rhapsody0128 <rhapsody0128@gmail.com>
2020-05-04 16:41:38 +0800

    linebot

--- 0428 node_js/line/index.js
+++ 0428 node_js/line/index.js
@@ -3,6 +3,7 @@ import linebot from 'linebot'
 import dotenvf from 'dotenv'
 // 引用dotenv套件
 import rp from 'request-promise'
+import cheerio from 'cheerio'
 
 dotenvf.config()
 // 讀取.env檔
@@ -13,20 +14,110 @@ const bot = linebot({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 })
 
-bot.on('message', async (event) => {
-  // 當收到訊息時
-
-  let msg = ''
+const img = async () => {
+  let re = {}
   try {
-    const data = await rp({ uri: 'https://kktix.com/events.json', json: true })
-    msg = data.entry[0].title
+    let found = ''
+    while (found.length === 0) {
+      const rand = Math.floor(Math.random() * 6704) + 1
+      const html = await rp({ uri: 'https://memes.tw/image/' + rand, json: true })
+      const $ = cheerio.load(html)
+      if ($('.text-center.mt-3 img').attr('src') !== undefined) {
+        found = $('.text-center.mt-3 img').attr('src')
+      }
+    }
+    re = {
+      type: 'image',
+      originalContentUrl: found,
+      previewImageUrl: found
+    }
   } catch (error) {
-    msg = '發生錯誤'
+    re = {
+      type: 'text',
+      text: '錯誤'
+    }
   }
-  event.reply(msg)
-})
+  return re
+}
+const month = async () => {
+  let re = {}
+  try {
+    let found = ''
+    // while (found.length === 0) {
+    // const rand = Math.floor(Math.random() * 271302) + 1
+    const html = await rp({ uri: 'https://memes.tw/wtf?sort=top-week', json: true })
+    const $ = cheerio.load(html)
+    for (let i = 0; i < $('.mb-3.border-bottom.pb-3').length; i++) {
+      console.log($('.mb-3.border-bottom.pb-3').eq(i).find('img').eq(0).attr('data-src'))
+    }
+
+    if ($('.text-center.mb-2 img').attr('src') !== undefined) {
+      found = $('.text-center.mb-2 img').attr('src')
+    }
+    // }
+
+    re = {
+      type: 'image',
+      originalContentUrl: found,
+      previewImageUrl: found
+    }
+  } catch (error) {
+    re = {
+      type: 'text',
+      text: '錯誤'
+    }
+  }
+  return re
+}
+month()
+const wtf = async () => {
+  let re = {}
+  try {
+    let found = ''
+    while (found.length === 0) {
+      const rand = Math.floor(Math.random() * 271302) + 1
+      const html = await rp({ uri: 'https://memes.tw/wtf/' + rand, json: true })
+      const $ = cheerio.load(html)
+
+      console.log($('.text-center.mb-2 img').attr('src'))
+      if ($('.text-center.mb-2 img').attr('src') !== undefined) {
+        found = $('.text-center.mb-2 img').attr('src')
+      }
+    }
+
+    re = {
+      type: 'image',
+      originalContentUrl: found,
+      previewImageUrl: found
+    }
+  } catch (error) {
+    re = {
+      type: 'text',
+      text: '錯誤'
+    }
+  }
+  return re
+}
 
 bot.listen('/', process.env.PORT, () => {
   // 在port啟動
   console.log('機器人已啟動')
+})
+
+bot.on('message', async (event) => {
+  // 當收到訊息時
+
+  let reply = {}
+  switch (event.message.text) {
+    case '隨機':
+      reply = await img()
+      break
+    case 'wtf':
+      reply = await wtf()
+      break
+    case 'month':
+      reply = await month()
+      break
+  }
+  event.reply(reply)
 })
