5612489906e3c922aff5fe946d80789190ff1cf0
Rhapsody0128 <rhapsody0128@gmail.com>
2020-05-18 11:56:33 +0800

    vue node 環境 card收集器

--- 0518 vue_cli/myvue/src/App.vue
+++ 0518 vue_cli/myvue/src/App.vue
@@ -1,27 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{text}}</h1>
+    <div id="cards">
+      <Card v-for="(card,index) in cards" :key="index" :data="card"></Card>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default {
-  name: 'App',
+  data () {
+    return {
+      text: '攝影作品',
+      cards: [
+        {
+          img: 'https://picsum.photos/400/200/?random=1',
+          title: '作品一'
+        },
+        {
+          img: 'https://picsum.photos/400/200/?random=2',
+          title: '作品二'
+        },
+        {
+          img: 'https://picsum.photos/400/200/?random=3',
+          title: '作品三'
+        },
+        {
+          img: 'https://picsum.photos/400/200/?random=4',
+          title: '作品四'
+        },
+        {
+          img: 'https://picsum.photos/400/200/?random=5',
+          title: '作品五'
+        },
+        {
+          img: 'https://picsum.photos/400/200/?random=6',
+          title: '作品六'
+        }
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    Card
   }
+
 }
 </script>
+<style>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
 </style>
---
+++ 0518 vue_cli/myvue/src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card">
+      <div class="card-img">
+        <img :src="data.img" alt=""></div>
+        <h1>{{data.title}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: Object
+  }
+}
+</script>
+
+<style>
+
+</style>
