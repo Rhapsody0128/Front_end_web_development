22f18f89ebde340074bded21dde1ac5affd61d70
Rhapsody0128 <rhapsody0128@gmail.com>
2020-06-16 22:53:37 +0800

    vue pomotomo

---
+++ 0526 vue_pomodoro/pomodoro/src/views/Home.vue
@@ -0,0 +1,168 @@
+<template>
+  <div id="home">
+    <h1>{{currentText}}</h1>
+    <div id="clock" class="d-flex w-75 m-auto">
+      <div id="totaltimebreakadjust">
+        <b-btn class="h-25 m-auto " variant='none'   @click="timebreakadjust('minus')">
+        <font-awesome-icon :icon="['fas','minus']"></font-awesome-icon>
+        </b-btn>
+        休息時間:{{totaltimebreak}}
+        <b-btn class="h-25 m-auto" variant='none'>
+          <font-awesome-icon @click="timebreakadjust('plus')"  :icon="['fas','plus']"></font-awesome-icon>
+        </b-btn>
+      </div>
+  <div>
+    <label for="range-1">Example range with min and max</label>
+    <b-form-input id="range-1" v-model="totaltime"  type="range" min="0" max="20"></b-form-input>
+    <div class="mt-2">Value: {{ value }}</div>
+  </div>
+      <radial-progress-bar v-if="!isBreak" class="m-auto" :diameter="400" :completed-steps="timeleft" :total-steps="totaltime" startColor="#eee"   stopColor="#e74" innerStrokeColor="#444">
+          <b-btn variant='primary' v-if='status!=1'     @click='start' >
+          <font-awesome-icon :icon="['fas','play']" ></font-awesome-icon>
+          </b-btn>
+          <b-btn  variant='danger' v-if='status==1'     @click='pause'>
+          <font-awesome-icon :icon="['fas','pause']" ></font-awesome-icon>
+          </b-btn>
+          <b-btn  variant='success' v-if='current.length > 0    || todos.length > 0' @click="finish(true)">
+          <font-awesome-icon :icon="['fas','step-forward']"     ></font-awesome-icon>
+          </b-btn>
+          {{timetext}}
+      </radial-progress-bar>
+      <radial-progress-bar v-else class="m-auto" :diameter="400" :completed-steps="timeleft" :total-steps="totaltimebreak" startColor="#ccc"   stopColor="#c11" innerStrokeColor="#333">
+          <b-btn variant='primary' v-if='status!=1'     @click='start' >
+          <font-awesome-icon :icon="['fas','play']" ></font-awesome-icon>
+          </b-btn>
+          <b-btn  variant='danger' v-if='status==1'     @click='pause'>
+          <font-awesome-icon :icon="['fas','pause']" ></font-awesome-icon>
+          </b-btn>
+          <b-btn  variant='success' v-if='current.length > 0    || todos.length > 0' @click="finish(true)">
+          <font-awesome-icon :icon="['fas','step-forward']"     ></font-awesome-icon>
+          </b-btn>
+          {{timetext}}
+      </radial-progress-bar>
+      <div id="totaltimeadjust">
+        <b-btn class="h-25 m-auto" variant='none'   @click="timeadjust('minus')">
+        <font-awesome-icon :icon="['fas','minus']"></font-awesome-icon>
+        </b-btn>
+        工作時間:{{totaltime}}
+        <b-btn class="h-25 m-auto" variant='none'>
+          <font-awesome-icon @click="timeadjust('plus')"  :icon="['fas','plus']"></font-awesome-icon>
+        </b-btn>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  data () {
+    return {
+      // status :
+      // 0 = 停止
+      // 1 = 播放
+      // 2 = 暫停
+      status: 0,
+      tiemer: 0
+    }
+  },
+  computed: {
+    currentText () {
+      return this.current.length > 0 ? this.current : this.todos.length > 0 ? '點擊開始' : '沒有事項'
+    },
+    timetext () {
+      const m = Math.floor(this.timeleft / 60)
+      const s = Math.floor(this.timeleft % 60)
+      return `${m} : ${s}`
+    },
+    alarm () {
+      return this.$store.getters.alarm
+    },
+    timeleft () {
+      return this.$store.getters.timeleft
+    },
+    current () {
+      return this.$store.getters.current
+    },
+    todos () {
+      return this.$store.getters.todos
+    },
+    totaltime () {
+      return this.$store.getters.totaltime
+    },
+    totaltimebreak () {
+      return this.$store.getters.totaltimebreak
+    },
+    isBreak () {
+      return this.$store.getters.isBreak
+    }
+
+  },
+  methods: {
+    start () {
+      if (this.status === 2) {
+      // 暫停後繼續
+        this.status = 1
+        this.timer = setInterval(() => {
+          this.$store.commit('countdown')
+          if (this.timeleft < 0) {
+            this.finish(false)
+          }
+        }, 1000)
+      } else {
+      // 開始
+        if (this.todos.length > 0) {
+          this.$store.commit('start')
+          this.status = 1
+          this.timer = setInterval(() => {
+            this.$store.commit('countdown')
+            if (this.timeleft < 0) {
+              this.finish(false)
+            }
+          }, 1000)
+        }
+      }
+    },
+    finish (skip) {
+      clearInterval(this.timer)
+      this.status = 0
+      this.$store.commit('finish')
+
+      if (!skip) {
+        const audio = new Audio()
+        audio.src = './alarms/' + this.alarm
+        audio.play()
+      }
+      if (this.todos.length > 0) {
+        this.start()
+      } else {
+        alert('結束')
+      }
+    },
+    pause () {
+      clearInterval(this.timer)
+      this.status = 2
+    },
+    timeadjust (i) {
+      if (this.status !== 1) {
+        if (i === 'minus') {
+          this.$store.commit('timeminus')
+        } else {
+          this.$store.commit('timeplus')
+        }
+      }
+    },
+    timebreakadjust (i) {
+      if (this.status !== 1) {
+        if (i === 'minus') {
+          this.$store.commit('timebreakminus')
+        } else {
+          this.$store.commit('timebreakplus')
+        }
+      }
+    }
+  }
+}
+</script>
